e46d16151c224b62a0a640e0c59080a11e13b847
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2023-09-10 22:32:56 +0530

    feat: added types to state

--- src/containers/home/DataProvider.tsx
+++ src/containers/home/DataProvider.tsx
@@ -1,6 +1,28 @@
 import React, { createContext, useContext, useState } from "react";
+type ContextValue = {
+  state: typeof initialValues;
+  setState: React.Dispatch<React.SetStateAction<typeof initialValues>>;
+};
+export type InitialValuesType = {
+  requisitionDetails: {
+    gender: string;
+    noOfOpenings: number;
+    requisitionTitle: string;
+    urgency: string;
+  };
+  jobDetails: {
+    jobDetails: string;
+    jobLocation: string;
+    jobTitle: string;
+  };
+  interviewSettings: {
+    interviewDuration: string;
+    interviewLanguage: string;
+    interviewMode: string;
+  };
+};
 
-const initialValues = {
+const initialValues: InitialValuesType = {
   requisitionDetails: {
     gender: "",
     noOfOpenings: 0,
@@ -18,16 +40,38 @@ const initialValues = {
     interviewMode: "",
   },
 };
+``
 
-const DataContext = createContext<{
-  state: typeof initialValues;
-  setState: React.Dispatch<React.SetStateAction<typeof initialValues>>;
-} | null>(null);
+
+// const initialValues = {
+//   requisitionDetails: {
+//     gender: "",
+//     noOfOpenings: 0,
+//     requisitionTitle: "",
+//     urgency: "",
+//   },
+//   jobDetails: {
+//     jobDetails: "",
+//     jobLocation: "",
+//     jobTitle: "",
+//   },
+//   interviewSettings: {
+//     interviewDuration: "",
+//     interviewLanguage: "",
+//     interviewMode: "",
+//   },
+// };
+const DataContext = createContext<ContextValue | null>(null);
+
+// const DataContext = createContext<{
+//   state: typeof initialValues;
+//   setState: React.Dispatch<React.SetStateAction<typeof initialValues>>;
+// } | null>(null);
 
 const DataProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [state, setState] = useState(initialValues);
+  const [state, setState] = useState<typeof initialValues>(initialValues);
 
   return (
     <DataContext.Provider value={{ state, setState }}>
